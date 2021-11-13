5993d8d0990e4135b8f7044a4625b1d04f541a90
Adrian M Ryan <adrian.m.ryan@antithesis.com>
2021-11-13 15:26:58 -0500

    adding basic comms

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,36 @@
+use serde_json::{Deserializer, Value};
+use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
-fn handle_client(stream: TcpStream) {
-    dbg!(stream);
+fn listen_to_connection(stream: TcpStream) {
+    dbg!(&stream);
+
+    let json_objs = Deserializer::from_reader(stream).into_iter::<Value>();
+
+    for value in json_objs {
+        println!("{}", value.unwrap());
+    }
 }
 
 fn main() -> std::io::Result<()> {
-    let args:Vec<String> = std::env::args().collect();
+    let args: Vec<String> = std::env::args().collect();
 
     for a in &args[1..] {
         println!("Connecting to {}", a);
-        let stream = TcpStream::connect(a);
-        dbg!(stream);
+        let mut stream = TcpStream::connect(a)?;
+        dbg!(&stream);
+        let packet = br#"{"foo": "bar"}"#;
+        loop {
+            stream.write_all(packet)?;
+            std::thread::sleep(std::time::Duration::from_secs(1));
+        }
     }
 
     let listener = TcpListener::bind("0.0.0.0:8080")?;
 
     // accept connections and process them serially
     for stream in listener.incoming() {
-        handle_client(stream?);
+        listen_to_connection(stream?);
     }
     Ok(())
 }
