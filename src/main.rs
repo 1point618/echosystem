cc31645e7698c8a30b3d515f91892e1cc951d949
Adrian M Ryan <adrian.m.ryan@antithesis.com>
2021-11-13 19:18:37 -0500

    starting to add argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::{App, Arg};
 use serde_json::{Deserializer, Value};
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
@@ -13,11 +14,28 @@ fn listen_to_connection(stream: TcpStream) {
 }
 
 fn main() -> std::io::Result<()> {
-    let args: Vec<String> = std::env::args().collect();
+    let args = App::new("echosystem")
+        .version("1")
+        .author("Adrian M Ryan")
+        .arg(Arg::new("echoer")
+            .short('e')
+            .long("echo_port")
+            .takes_value(true),
+        )
+        .arg(Arg::new("yodelers")
+            .short('y')
+            .long("yodel_ips")
+            .multiple_values(true),
+        )
+        .get_matches();
 
-    for a in &args[1..] {
+    let echo_port: i64 = args.value_of_t("echoer").unwrap_or(8000);
+
+    let yodel_ips: Vec<&str> = args.values_of_t::<&str>("yodelers").unwrap_or(vec!("localhost:8000"));
+
+    for a in yodel_ips {
         let a = a.clone();
-        std::thread::spawn( move || {
+        std::thread::spawn(move || {
             println!("Connecting to {}", a);
             let mut stream = TcpStream::connect(a).unwrap();
             dbg!(&stream);
@@ -29,11 +47,11 @@ fn main() -> std::io::Result<()> {
         });
     }
 
-    let listener = TcpListener::bind("0.0.0.0:8000")?;
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", echo_port))?;
 
     // accept connections and process them serially
     for stream in listener.incoming() {
-        std::thread::spawn( move || {
+        std::thread::spawn(move || {
             listen_to_connection(stream.unwrap());
         });
     }
